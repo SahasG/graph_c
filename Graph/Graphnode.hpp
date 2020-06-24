906d8189ee07beca33ea5748cd7bf51ea14c35cc
SahasG <sahasgelli@Sahass-MacBook-Pro.local>
2020-06-24 16:39:48 -0400

    Graphnode hpp file

---
+++ Graph/Graphnode.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <set>
+
+class Graphnode{
+
+	std::string data;
+	std::list<Graphnode> adjacentVertices;
+public:
+
+	Graphnode();
+	std:string getData();
+	void addAdjacentVertex(Graphnode newNode);
+	void removeAdjacentVertex(Graphnode removeNode);
+	std:list<Graphnode> getAdjacentVertex();
+	std::list<String> getStringAdjacentVertex();
+
+}
